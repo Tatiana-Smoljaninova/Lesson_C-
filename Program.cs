56f51f68664208f8a96ce2fae2936c6a8854498f
Tatiana <tsmall@rambler.ru>
2023-01-20 20:11:02 +0600

    Lesson_C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значение");
+int x = Convert.ToInt32(Console.ReadLine());
+x ++;
+Console.Write($"Мы ввели {x}");
+
+if (x>100)
+Console.Write($"Больше 100 {x}");
+else
+Console.Write($"Меньше 100 {x}");
+if (true)
+{
+    
+}
